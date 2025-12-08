66a6e9a4d6d44a820eb4a255545da3d3ec41e422
levibliz <levibliss2000@gmail.com>
2025-12-08 13:25:54 +0100

    Updated gitignore

--- frontend/.next/dev/static/chunks/app_dashboard_page_tsx_5d8a8646._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_84964ca7._.js",
-  "static/chunks/app_dashboard_21e51193._.js"
-],
-    source: "dynamic"
-});
